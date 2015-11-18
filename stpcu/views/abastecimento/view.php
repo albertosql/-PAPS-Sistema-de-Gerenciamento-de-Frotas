fdfc7a43043bab4e1e1dbcb84c546ed991ced2e5
Ingrid do Nascimento Mendes <ingrid.mendes.si@gmail.com>
2015-11-18 19:35:25 -0300

    alerações interface

--- stpcu/views/abastecimento/view.php
+++ stpcu/views/abastecimento/view.php
@@ -6,7 +6,7 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Abastecimento */
 
-$this->title = $model->id;
+$this->title = 'Visualizar Abastecimento';
 $this->params['breadcrumbs'][] = ['label' => 'Abastecimentos', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -15,11 +15,11 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('Atualizar', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Deletar', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => 'Deseja confirmar a exclusão desse item?',
                 'method' => 'post',
             ],
         ]) ?>
@@ -28,7 +28,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
+            //'id',
             'preco_litro',
             'id_posto',
             'id_veiculo',
