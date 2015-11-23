850dab6cecd3f2837eb2b26c913f5289d11aa405
Ingrid do Nascimento Mendes <ingrid.mendes.si@gmail.com>
2015-11-23 16:39:29 -0300

    alteração do tamanho do input

--- stpcu/views/cor/_form.php
+++ stpcu/views/cor/_form.php
@@ -12,7 +12,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'nome')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'nome')->textInput(['maxlength'=>10,'style'=>'width:200px']);?>
 
     <div class="form-group">
 
